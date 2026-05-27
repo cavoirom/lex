8b1c9c3291fc61268a08c10d9a833955bfa3fa77
Cavoi Rom <cavoirom.wanted@yahoo.com>
2026-05-28 04:12:54 +0700

    lex: updated dev.sh to match home enviroment.

--- dev.sh
+++ dev.sh
@@ -7,14 +7,14 @@ guix shell --container \
     --share="$SCRIPT_DIRECTORY/.container-home/.config/amp"="$HOME/.config/amp" \
     --share="$SCRIPT_DIRECTORY/.container-home/.local/share/amp"="$HOME/.local/share/amp" \
     --share="$SCRIPT_DIRECTORY/.container-home/.zshrc"="$HOME/.zshrc" \
-    --share="$SCRIPT_DIRECTORY" \
-    --expose="$HOME/.local/share/terminfo" \
+    --share="$SCRIPT_DIRECTORY/.." \
+    --expose="$HOME/.terminfo" \
     --expose='/gnu/store' \
-    --preserve='^TERMINFO_DIRS$' \
+    --preserve='^TERMINFO$' \
     --preserve='^TERM$' \
     --network \
     --no-cwd \
     --emulate-fhs \
     --cwd="$SCRIPT_DIRECTORY" \
     --manifest="$SCRIPT_DIRECTORY/manifest.scm" \
-    -- env PROMPT="%F{red}┌─╼[%F{green}%m:%2~%F{red}(env)]"$'\n'"└╼ %{%f%}" zsh -l
+    -- env PROMPT="%F{red}┌─╼[%F{green}%m:%2~%F{red}(dev)]"$'\n'"└╼ %{%f%}" zsh -l
